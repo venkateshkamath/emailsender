d3d1a45f4ff2e3061c42cf1ac772e54ad0a54c6e
venkateshkamath <venkykamath2000@gmail.com>
2022-09-07 17:57:39 +0530

    changed the credentials

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -8,14 +8,14 @@ const sendEmail = async (req, res) => {
     host: "smtp.ethereal.email",
     port: 587,
     auth: {
-      user: "ebba18@ethereal.email",
-      pass: "jdsYr9a9f2K4VEjxkc",
+      user: "ebba18@ethereals.email",
+      pass: "ssfnsfjsnfjsnfjshdhwdjawn",
     },
   });
 
   let info = await transporter.sendMail({
-    from: '"Shreyas" <shreyassp17@outlook.com>', // sender address
-    to: "shreyassp17@gmail.com, rakshithaacharya981@gmail.com", // list of receivers
+    from: '"Shreyas Test" <test@ethereal.com>', // sender address
+    to: "test@gmail.com, steve.jobs@gmail.com", // list of receivers
     // to: "bar@example.com",
     subject: "Hello ✔", // Subject line
 
@@ -27,8 +27,8 @@ const sendEmail = async (req, res) => {
 const sendGrid = async (req, res) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: "shreyassp17@gmail.com", // Change to your recipient
-    from: "venkykamath08@gmail.com", // Change to your verified sender
+    to: "test@gmail.com", // Change to your recipient
+    from: "johndoe@gmail.com", // Change to your verified sender
     subject: "Test",
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
